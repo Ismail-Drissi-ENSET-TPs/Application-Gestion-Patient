9bfe93f6fe427ca518fa0630b68b831856c14237
ismaildrs <drissiisismail@gmail.com>
2025-03-17 11:22:49 +0000

    l'ajout de la pagination et amélioration de l'IU

--- src/main/java/com/enset/tp/appgestionpatient/web/PatientController.java
+++ src/main/java/com/enset/tp/appgestionpatient/web/PatientController.java
@@ -3,9 +3,12 @@ package com.enset.tp.appgestionpatient.web;
 import com.enset.tp.appgestionpatient.entities.Patient;
 import com.enset.tp.appgestionpatient.repositories.PatientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,9 +19,11 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("listPatients", patients);
+    public String index(Model model, @RequestParam(name = "p", defaultValue = "0") int page, @RequestParam(name = "s", defaultValue = "4") int size) {
+        Page<Patient> patients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("listPatients", patients.getContent());
+        model.addAttribute("pages", new int[patients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "patients";
     }
 
