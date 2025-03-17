2d7d5621f2920db680039b4a7b540969288b7087
ismaildrs <drissiisismail@gmail.com>
2025-03-17 09:35:05 +0000

    structure du project

---
+++ src/main/java/com/enset/tp/appgestionpatient/AppGestionPatientApplication.java
@@ -0,0 +1,13 @@
+package com.enset.tp.appgestionpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestionPatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppGestionPatientApplication.class, args);
+    }
+
+}
